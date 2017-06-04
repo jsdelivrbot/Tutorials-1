965a298990d22ed49d9aa00892828012cdcf3c85
Walt Tran <wtran@ucdavis.edu>
2017-06-04 10:19:09 -0700

    AlbumDetail styling on View tags. Added flex-box styling

--- React-Native-Tutorials/albums/src/components/AlbumDetail.js
+++ React-Native-Tutorials/albums/src/components/AlbumDetail.js
@@ -1,13 +1,66 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
 
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+  //destructuring so we dont have to type props.album.title, etc
+  const { title, artist, thumbnail_image, image } = album;
+  const {
+    headerContentStyle,
+    thumbnailStyle,
+    thumbnailContainerStyle,
+    headerTextStyle,
+    imageStyle
+  } = styles;
+
+
   return (
-    <View>
-      <Text>{props.album.title}</Text>
-    </View>
+    <Card>
+      <CardSection>
+      <View style={thumbnailContainerStyle}>
+        <Image
+          source={{ uri: thumbnail_image }}
+          style={thumbnailStyle}
+        />
+      </View>
+      <View style={headerContentStyle}>
+        <Text style={headerTextStyle}>{title}</Text>
+        <Text>{artist}</Text>
+      </View>
+      </CardSection>
+      <CardSection>
+        <Image style={imageStyle} source={{ uri: image }} />
+      </CardSection>
+
+    </Card>
   );
+};
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  headerTextStyle: {
+    fontSize: 18
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
+  }
 };
 
 export default AlbumDetail;
