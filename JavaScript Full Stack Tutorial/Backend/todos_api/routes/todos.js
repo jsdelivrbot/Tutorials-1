0c66044919948be731f13eb53fc7bc2476ed1ce6
waru <wtran@ucdavis.edu>
2017-11-06 13:54:08 -0800

    delete route

--- JavaScript Full Stack Tutorial/Backend/todos_api/routes/todos.js
+++ JavaScript Full Stack Tutorial/Backend/todos_api/routes/todos.js
@@ -41,4 +41,14 @@ router.put('/:todoId', function(req, res) {
       res.send(err);
     })
 });
+
+router.delete('/:todoId', function(req, res) {
+  db.Todo.remove({_id: req.params.todoId})
+    .then(function() {
+      res.json({messsage: 'Item Deleted'});
+    })
+    .catch(function(err) {
+      res.send(err);
+    })
+});
 module.exports = router;
